2ecc0b7ac70ca537d6b268cf8ee72470ee98d67d
marcellui <marcellui123@gmail.com>
2023-11-08 16:19:45 -0500

    add get request for single warehouse

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,5 +1,22 @@
 const express = require("express");
 
 const router = express.Router();
+const knex = require("knex")(require("../knexfile"));
+
+router.get("/:id", async (req, res) => {
+    try {
+        const foundWarehouse = await knex("warehouses").where({ id: req.params.id });
+
+        if (foundWarehouse.length === 0) {
+            return res.status(404).json({
+                message: `Warehouse with ID ${req.params.id} not found`,
+            });
+        }
+        return res.status(200).json(foundWarehouse);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error - self destruct" });
+    }
+});
 
 module.exports = router;
