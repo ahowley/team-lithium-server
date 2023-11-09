808ff2ff2cd07511472947b96a34359a39627e2b
Jordan <73490368+CNjordan@users.noreply.github.com>
2023-11-09 10:55:05 -0500

    GET inventory items

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,5 +1,14 @@
+const fs = require("fs");
 const express = require("express");
-
 const router = express.Router();
+const { v4: uuid } = require("uuid");
+const inventoryController = require("../controllers/inventoryControllers");
 
+router
+    .route("/")
+    .get(inventoryController.getAllInventory)
+    
+router
+    .route("/:inventory_id")
+    .get(inventoryController.getSingleInventory)
 module.exports = router;
